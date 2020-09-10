e2cb335ef18f7e0ce3eb19d05d82fb937fee16c4
zhaoboqiang1 <zhaoboqiang1@kingsoft.com>
2020-09-10 19:27:14 +0800

    Move directory.

---
+++ Assets/ToLua/Editor/ToLuaSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LuaInterface
+{
+
+    public interface ToLuaSettings
+    {
+        string saveDir { get; }
+        string toluaBaseType { get; }
+        string baseLuaDir { get; }
+        string injectionFilesPath { get; }
+
+        string IncludedAssemblyCsv { get; } 
+        string IncludedNamespaceCsv { get; } 
+        string IncludedTypeCsv { get; } 
+    }
+}
