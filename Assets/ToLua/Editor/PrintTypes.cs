09e09e111a09c103b8e79a2e88bcab951cb9dacc
Zhao Boqiang <beckzar@hotmail.com>
2020-09-24 10:54:41 +0800

    Debug types.

--- Assets/ToLua/Editor/PrintTypes.cs
+++ Assets/ToLua/Editor/PrintTypes.cs
@@ -7,12 +7,21 @@ namespace LuaInterface.Editor
 {
     public static class PrintTypes
     {
-        private static void SaveCsv(List<Type> types)
+        public static void SaveCsv(List<Type> types, string fileName)
         {
-            var lines = new List<string> { "FullName,Namespace,Name,Public,NestedPublic,Generic,Abstract" };
+            var lines = new List<string> { "FullName,Namespace,Name,Included,NamespaceIncluded,InTypeCsv,TypeIncluded,Unsupported,Public,Visible,NestedPublic,Generic,Abstract" };
             foreach (var type in types)
-                lines.Add($"{type.FullName},{type.Namespace},{type.Name},{type.IsPublic},{type.IsNestedPublic},{type.IsGenericType},{type.IsAbstract}");
-            ReflectUtility.SaveCsv(lines, Application.dataPath + "/Editor/all_types.csv");
+            {
+                var included = ToLuaSettingsUtility.IsIncluded(type);
+                var namespaceIncluded = ToLuaSettingsUtility.IsNamespaceIncluded(type.Namespace);
+                var inTypeCsv = ToLuaSettingsUtility.InIncludedTypeCsv(type);
+                var typeIncluded = ToLuaSettingsUtility.IsTypeIncluded(type);
+                var isPublic = ToLuaTypes.IsPublic(type);
+                var isUnsupport = ToLuaTypes.IsUnsupported(type);
+
+                lines.Add($"{type.FullName},{type.Namespace},{type.Name},{included},{namespaceIncluded},{inTypeCsv},{typeIncluded},{isUnsupport},{isPublic},{type.IsVisible},{type.IsNestedPublic},{type.IsGenericType},{type.IsAbstract}");
+            }
+            ReflectUtility.SaveCsv(lines, $"{Application.dataPath}/Editor/{fileName}.csv");
         }
 
         [MenuItem("Print/Types")]
@@ -29,7 +38,7 @@ namespace LuaInterface.Editor
                 }
             }
 
-            SaveCsv(types);
+            SaveCsv(types, "all_types");
         }
     }
 }
