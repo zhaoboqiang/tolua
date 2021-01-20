16767a5ef6dc91851693fdba31ace606f7d3aa91
Zhao Boqiang <beckzar@hotmail.com>
2021-01-20 10:46:52 +0800

    Add using example.

---
+++ Assets/Examples/25_Using/TestUsing.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.IO;
+using LuaInterface;
+
+//use menu Lua->Copy lua files to Resources. 之后才能发布到手机
+[NoToLua]
+public class TestUsing : LuaClient 
+{
+    string tips = "Test Using";
+
+    protected override LuaFileUtils InitLoader()
+    {
+        return new LuaResLoader();
+    }
+
+    protected override void CallMain()
+    {
+        var func = L.GetFunction("Test");
+        func.Call();
+        func.Dispose();
+    }
+
+    protected override void StartMain()
+    {
+        L.DoFile("TestUsing.lua");
+        CallMain();
+    }
+
+    new void Awake()
+    {
+        Application.logMessageReceived += Logger;
+        base.Awake();
+    }
+
+    new void OnApplicationQuit()
+    {
+        base.OnApplicationQuit();
+
+        Application.logMessageReceived -= Logger;
+    }
+
+    void Logger(string msg, string stackTrace, LogType type)
+    {
+        tips += msg;
+        tips += "\r\n";
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(Screen.width / 2 - 200, Screen.height / 2 - 200, 400, 400), tips);
+    }
+}
