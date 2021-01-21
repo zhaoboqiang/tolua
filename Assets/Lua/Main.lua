f2f3f45929f04552c63cd463fbfdf813c2f0a7c8
Zhao Boqiang <beckzar@hotmail.com>
2021-01-21 14:57:53 +0800

    Override require and and using to it.

--- Assets/Lua/Main.lua
+++ Assets/Lua/Main.lua
@@ -1,6 +1,42 @@
+local function get_lib_from_name_path(name_path)
+	local path = name_path
+	local delimiter = name_path:find('<')
+	if delimiter ~= nil then
+		path = name_path:sub(1, delimiter - 1)
+	end
+	local names = path:split('.')
+	local count = #names;
+	local t = _G
+	for index = 1, count - 1 do
+		t = t[names[index]]
+	end
+	local name = names[count]
+	if delimiter ~= nil then
+		name = name .. name_path:sub(delimiter)
+	end
+	return t[name]
+end
+
+local raw_require = require
+local function prequire(modname)
+    local status, lib = pcall(raw_require, modname)
+    if status then
+        return lib
+    end
+
+	local status = using(modname)
+	if status then
+		return get_lib_from_name_path(modname)
+	end
+
+    print(debug.traceback(lib))
+    return nil
+end
+require = prequire
+
 --主入口函数。从这里开始lua逻辑
-function Main()					
-	print("logic start")	 		
+function Main()
+	print("logic start")
 end
 
 --场景切换通知
